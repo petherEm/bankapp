a688da76545828713da8a2fde4c5c0e48d79cb59
Piotr Maciejewski <pm@piotrs-macbook-pro.home>
2022-08-29 18:25:21 +0200

    updated

--- components/CTA.jsx
+++ components/CTA.jsx
@@ -6,7 +6,7 @@ const CTA = () => {
     <section className="flex justify-center items-center sm:my-16 my-6 sm:px-16 px-6 sm:py-12 py-4 sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow">
       <div className="flex-1 flex flex-col">
         <h2 className="font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full">
-          Let's try our service now!
+          Try our service now!
         </h2>
         <p className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] max-w-[470px] mt-5">
           Everything you need to accept card payments and grow your business
